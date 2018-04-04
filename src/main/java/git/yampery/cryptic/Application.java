f1d7feac53e7883a408a4a20f7c443d5ce229e31
Yampery <yampery@163.co>
2018-04-04 14:09:45 +0800

    更改==> 主类添加mapper扫描

--- src/main/java/git/yampery/cryptic/Application.java
+++ src/main/java/git/yampery/cryptic/Application.java
@@ -1,9 +1,11 @@
 package git.yampery.cryptic;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("git.yampery.cryptic.dao")
 public class Application {
 
 	public static void main(String[] args) {
